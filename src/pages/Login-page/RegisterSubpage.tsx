03421671525c83bd468e03d5b18b26671453e71c
KRook0110 <shawnbacktoschool@gmail.com>
2024-10-06 19:32:16 +0700

    Added Shadcn and changed tsconfig to reroute baseurl and import aliases

--- src/pages/Login-page/RegisterSubpage.tsx
+++ src/pages/Login-page/RegisterSubpage.tsx
@@ -1,3 +1,4 @@
+
 function RegisterSubpage() {
 
     return (<>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,36 +2,68 @@
 const colors = require("tailwindcss/colors");
 
 export default {
-  content: ["./index.html", "./src/**/*.{js,jsx,tsx,ts}"],
+    darkMode: ["class"],
+    content: ["./index.html", "./src/**/*.{js,jsx,tsx,ts}"],
   theme: {
-    fontFamily: {
-      display: ["Poppins", "sans-serif"],
-      body: ["Questrial", "sans-serif"],
-    },
-    extend: {
-      borderRadius: {
-        DEFAULT: "8px",
-        lg: "16px"
-      },
-      colors: {
-        primary: {
-          light: "#3089D6",
-          DEFAULT: "#1C429A",
-          dark: "#03081B",
-        },
-        secondary: {
-          light: "#FFD690",
-          DEFAULT: "#FFBD5A",
-          dark: "#EDA740",
-        },
-        // background: {
-        //     DEFAULT: "#f5f5f5",
-        //     900 :
-        // }
-        background: colors.neutral,
-        alert: colors.red,
-      },
-    },
+  	fontFamily: {
+  		display: ["Poppins", "sans-serif"],
+  		body: ["Questrial", "sans-serif"]
+  	},
+  	extend: {
+  		borderRadius: {
+  			DEFAULT: '8px',
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		},
+  		colors: {
+  			primary: {
+  				light: '#3089D6',
+  				DEFAULT: 'hsl(var(--primary))',
+  				dark: '#03081B',
+  				foreground: 'hsl(var(--primary-foreground))'
+  			},
+  			secondary: {
+  				light: '#FFD690',
+  				DEFAULT: 'hsl(var(--secondary))',
+  				dark: '#EDA740',
+  				foreground: 'hsl(var(--secondary-foreground))'
+  			},
+  			background: 'hsl(var(--background))',
+  			alert: 'colors.red',
+  			foreground: 'hsl(var(--foreground))',
+  			card: {
+  				DEFAULT: 'hsl(var(--card))',
+  				foreground: 'hsl(var(--card-foreground))'
+  			},
+  			popover: {
+  				DEFAULT: 'hsl(var(--popover))',
+  				foreground: 'hsl(var(--popover-foreground))'
+  			},
+  			muted: {
+  				DEFAULT: 'hsl(var(--muted))',
+  				foreground: 'hsl(var(--muted-foreground))'
+  			},
+  			accent: {
+  				DEFAULT: 'hsl(var(--accent))',
+  				foreground: 'hsl(var(--accent-foreground))'
+  			},
+  			destructive: {
+  				DEFAULT: 'hsl(var(--destructive))',
+  				foreground: 'hsl(var(--destructive-foreground))'
+  			},
+  			border: 'hsl(var(--border))',
+  			input: 'hsl(var(--input))',
+  			ring: 'hsl(var(--ring))',
+  			chart: {
+  				'1': 'hsl(var(--chart-1))',
+  				'2': 'hsl(var(--chart-2))',
+  				'3': 'hsl(var(--chart-3))',
+  				'4': 'hsl(var(--chart-4))',
+  				'5': 'hsl(var(--chart-5))'
+  			}
+  		}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
